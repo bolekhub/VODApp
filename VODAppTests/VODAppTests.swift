7decee3f550dbd9a4d3753f427acb3a72b2d5efd
Boris Chirino <bolek1976@yahoo.com>
2019-07-22 09:53:24 +0200

    Initial Commit

---
+++ VODAppTests/VODAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  VODAppTests.swift
+//  VODAppTests
+//
+//  Created by Boris Chirino on 22/07/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+import XCTest
+@testable import VODApp
+
+class VODAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
